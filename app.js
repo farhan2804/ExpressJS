f69763534d5a826bbc60bf11e1d8f08ce1d5ddcc
farhan <el3317.farhanmahmood@gmail.com>
2024-03-26 21:25:58 +0530

    added app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-// const app = express();
-// app.get("/", (req, res) => {
-//   res.send("hello from the server home page");
-// });
-// app.get("/about", (req, res) => {
-//   res.send("hello from the about page ");
-// });
-// app.listen(5000, () => {
-//   console.log("listening to port 5000");
-// });
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello from the server home page");
+});
+app.get("/about", (req, res) => {
+  res.send("hello from the about page ");
+});
+app.listen(5000, () => {
+  console.log("listening to port 5000");
+});

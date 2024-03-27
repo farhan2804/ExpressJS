8db1717b104a3bad332f97c7609d4f57162708dd
farhan <el3317.farhanmahmood@gmail.com>
2024-03-27 16:25:50 +0530

    use of template engine in Express JS

--- src/index.js
+++ src/index.js
@@ -1,16 +1,52 @@
 const express = require("express");
 const app = express();
-const path=require("path");
-const staticPath=path.join(__dirname,"../public")
-console.log(staticPath);
+const path=require('path');
+const port = 5000;
+const staticPath=path.join(__dirname,"../public");
+app.set('view engine', 'hbs');
+
 app.use(express.static(staticPath));
 
+//the below code is for template engines
+//we need to set the vie engine
+
+//template engine route
+app.get("/", (req, res) => {
+  res.render("index", {
+    userName: "Farhan",
+  });
+});
+
+app.listen(5000, () => {
+  console.log(`server started at ${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+//below code is an example of serving static content
+
+// const path=require("path");
+// const staticPath=path.join(__dirname,"../public")
+// // console.log(staticPath);
+
+// app.get("/", (req, res) => {
+//     res.send("hello mannu");
+//   });
 // app.get("/", (req, res) => {
 //   res.send("hello from the server home page");
 // });
+// // app.use(express.static(staticPath));
+
 // app.get("/about", (req, res) => {
 //   res.send("hello from the about page ");
 // });
-app.listen(5000, () => {
-  console.log("listening to port 5000");
-});
+// app.listen(5000, () => {
+//   console.log("listening to port 5000");
+// });
